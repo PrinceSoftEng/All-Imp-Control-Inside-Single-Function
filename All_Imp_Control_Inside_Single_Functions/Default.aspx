bc1042d87363c316e00dd6ccdc63ecf8f6b0289c
Sandeep Nikalje <sandeepn@techlink.co.in>
2023-08-02 11:36:46 +0530

    Add project files.

---
+++ All_Imp_Control_Inside_Single_Functions/Default.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="All_Imp_Control_Inside_Single_Functions.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+            padding: 5px
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2
+        }
+
+        tr:hover {
+            background-color: #d0dd7a
+        }
+
+        .container {
+            display: flex;
+            flex-direction: row;
+            align-items:start;
+        }
+
+        .control {
+            margin: 0px;
+            padding:50px;
+            border: 0px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="control">
+                <b>
+                    <asp:Label ID="lblDropDownList" runat="server" Text="DropDownList:"></asp:Label></b>
+                <asp:DropDownList ID="ddlEmployee" runat="server"></asp:DropDownList><br />
+            </div>
+            <div class="control">
+                <b>
+                    <asp:Label ID="lblCheckboxlist" runat="server" Text="Checkboxlist:"></asp:Label></b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:CheckBoxList ID="chkEmployee" runat="server"></asp:CheckBoxList>
+            </div>
+            <div class="control">
+                <b>
+                    <asp:Label ID="lblRadioButtonList" runat="server" Text="RadioButtonList:"></asp:Label></b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:RadioButtonList ID="rblEmployee" runat="server"></asp:RadioButtonList>
+            </div>
+        </div>
+        <hr />
+        <div>
+            <b>
+                <asp:Label ID="lblGridView" runat="server" Text="GridView:"></asp:Label></b>
+            <asp:GridView ID="gvEmployee" runat="server"></asp:GridView>
+        </div>
+        <br />
+        <div>
+            <b>
+                <asp:Label ID="lblRepeater" runat="server" Text="Repeater:"></asp:Label></b>
+            <asp:Repeater ID="rptEmployee" runat="server">
+                <HeaderTemplate>
+                    <table>
+                        <tr>
+                            <th>Id</th>
+                            <th>First Name</th>
+                            <th>Middle Name</th>
+                            <th>Last Name</th>
+                            <th>Email Id</th>
+                        </tr>
+                </HeaderTemplate>
+                <ItemTemplate>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lblId" runat="server" Text='<%#Eval("Id") %>'></asp:Label></td>
+                        <td>
+                            <asp:Label ID="lblFName" runat="server" Text='<%#Eval("FName") %>'></asp:Label></td>
+                        <td>
+                            <asp:Label ID="lblMName" runat="server" Text='<%#Eval("MName") %>'></asp:Label></td>
+                        <td>
+                            <asp:Label ID="lblLName" runat="server" Text='<%#Eval("LName") %>'></asp:Label></td>
+                        <td>
+                            <asp:Label ID="lblEmailId" runat="server" Text='<%#Eval("EmailId") %>'></asp:Label>
+                        </td>
+                    </tr>
+                </ItemTemplate>
+                <FooterTemplate>
+                    </table>
+                </FooterTemplate>
+            </asp:Repeater>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ All_Imp_Control_Inside_Single_Functions/Default.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace All_Imp_Control_Inside_Single_Functions
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindAll(ddlEmployee, "Select Id as value, FName as text from Employee");
+            BindAll(chkEmployee, "Select Id as value, FName as text from Employee");
+            BindAll(rblEmployee, "Select Id as value, FName as text from Employee");
+            BindAll(gvEmployee, "Select Id,FName,MName,LName,EmailId from Employee");
+            BindAll(rptEmployee, "Select Id,FName,MName,LName,EmailId from Employee");
+        }
+
+        public static void BindAll(object c, string sql)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataSet ds = new DataSet())
+                        {
+                            sda.Fill(ds);
+                            Control item = (Control) c;
+                            if (item is DropDownList)
+                            {
+                                DropDownList ddlEmployee1 = (DropDownList)item;
+                                ddlEmployee1.Items.Clear();
+                                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                                {
+                                    ddlEmployee1.Items.Insert(i, new ListItem(ds.Tables[0].Rows[i]["text"].ToString(), ds.Tables[0].Rows[i]["value"].ToString()));
+                                }
+                                ddlEmployee1.Items.Insert(0, new ListItem("--Select Data--", "0"));
+                            }
+                            if (item is GridView)
+                            {
+                                ((GridView)item).DataSource = ds;
+                                ((GridView)item).DataBind();
+                            }
+                            if (item is CheckBoxList)
+                            {
+                                CheckBoxList chklist= (CheckBoxList)item;
+                                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                                {
+                                    chklist.Items.Insert(i, new ListItem(ds.Tables[0].Rows[i]["text"].ToString(), ds.Tables[0].Rows[i]["value"].ToString()));
+                                }
+                            }
+                            if (item is RadioButtonList)
+                            {
+                                RadioButtonList rblemployee = (RadioButtonList)item;
+                                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                                {
+                                    rblemployee.Items.Insert(i, new ListItem(ds.Tables[0].Rows[i]["text"].ToString(), ds.Tables[0].Rows[i]["value"].ToString()));
+                                }
+                            }
+                            if (item is Repeater)
+                            {
+                                ((Repeater)item).DataSource = ds;
+                                ((Repeater)item).DataBind();
+                            }
+
+                            if (item is DataList)
+                            {
+                                ((DataList)item).DataSource = ds;
+                                ((DataList)item).DataBind();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
